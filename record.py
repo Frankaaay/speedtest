71d8e5f4f724c465dd7ec83880044987f70a1668
epiglottis <epiglottis.cartilage@gmail.com>
2024-08-24 18:21:43 +0800

    roomid and platform

--- record.py
+++ record.py
@@ -20,32 +20,44 @@ def main():
     now = datetime.datetime.now().strftime("%m-%d_%H-%M")
     os.makedirs(f"log/{now}/",exist_ok=True)
 
-    pings = Log(open(f"log/{now}/ping.csv",'w',encoding='utf-8-sig'))
-    lives = live.Reporter(open(f"log/{now}/stuck.csv",'w',encoding='utf-8-sig'))
+    ping_log = Log(open(f"log/{now}/ping.csv",'w',encoding='utf-8-sig'))
+    live_console = live.Console()
+    live_log = live.Reporter(open(f"log/{now}/stuck.csv",'w',encoding='utf-8-sig'),threshold=3)
 
     browser = input("浏览器？ (Edge/Chrome/Firefox): ").title()
     platform = input("平台？ [b]B站/[d]抖音/[x]西瓜/[a]爱奇艺").lower()
+    room_id = input("房间号：？ (可不填)").strip()
+
+    if len(room_id) == 0:
+        room_id = None
+
     if platform == 'b':
-        living = live.BiliLive(browser, False)
+        living = live.BiliLive(browser, False, room_id)
     elif platform == 'd':
-        living = live.DouyinLive(browser, False)
+        living = live.DouyinLive(browser, False, room_id)
     else:
-        living = live.BiliLive(browser, False)
+        living = live.BiliLive(browser, False, room_id)
     
 
     try: 
         while True:
             now = datetime.datetime.now()
-            pings.record(now.strftime("%m-%d %H:%M:%S"),stable.ping('192.168.0.1'),stable.ping('www.baidu.com'))
-            lives.record(*living.check())
+            ping_log.record(now.strftime("%m-%d %H:%M:%S"),stable.ping('192.168.0.1'),stable.ping('www.baidu.com'))
+            live_console.record(*living.check())
+            live_log.record(*living.check())
+
             time.sleep(1 - (now.microsecond/1000000))
+
+            if now.second == 0:
+                live_log.flush()
+                ping_log.flush()
     except KeyboardInterrupt:
         pass
     finally:
         print("Exiting...")
         living.quit()
-        lives.flush()
-        pings.flush()
+        live_log.flush()
+        ping_log.flush()
         
     
 
