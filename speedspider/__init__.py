5ee9e37be9dc873f066f45ee1c03b318d576737c
epiglottis <epiglottis.cartilage@gmail.com>
2024-09-02 09:22:08 +0800

    speedtest now start a new browser each time

--- speedspider/__init__.py
+++ speedspider/__init__.py
@@ -34,31 +34,34 @@ URLS = [
 class SpeedTester(Producer):
     def __init__(self, headless=True, timeout=timedelta(minutes=2), urls=URLS):
         super().__init__()
-        self.driver = web_driver(headless)
-        self.driver.implicitly_wait(5)
+        self.headless = headless
         self.timeout = timeout.total_seconds()
         self.urls = urls
 
     def update(self):
         super().update()
+        driver = web_driver(self.headless)
+        driver.implicitly_wait(5)
         url = random.choice(self.urls)
-        self.driver.get(url)
-        startStopBtn = self.driver.find_element(By.ID, "startStopBtn")
+        driver.get(url)
+        startStopBtn = driver.find_element(By.ID, "startStopBtn")
         if startStopBtn.get_attribute("class") == "":
             startStopBtn.click()
         try:
-            WebDriverWait(self.driver, self.timeout).until(
+            WebDriverWait(driver, self.timeout).until(
                 lambda driver: int(driver.execute_script("return s.getState()")) == 4)
-            lag = self.driver.find_element(By.ID, "pingText").text
-            jit = self.driver.find_element(By.ID, "jitText").text
-            dl = self.driver.find_element(By.ID, "dlText").text
-            ul = self.driver.find_element(By.ID, "ulText").text
+            lag = driver.find_element(By.ID, "pingText").text
+            jit = driver.find_element(By.ID, "jitText").text
+            dl = driver.find_element(By.ID, "dlText").text
+            ul = driver.find_element(By.ID, "ulText").text
         except SEexceptions.NoSuchElementException:
             lag = jit = dl = ul = "nan"
         except SEexceptions.TimeoutException:
             lag = jit = dl = ul = "nan"
         except Exception as e:
             lag = jit = dl = ul = str(e)
+        finally:
+            driver.quit()
 
         try:
             self.res = SpeedTestResult(float(lag), float(jit), float(dl), float(ul))
